e1168b71b7fe0fc7e0fa3d885960e3bc380713e6
feztix <pan0v.s.s@yandex.ru>
2020-11-17 09:28:26 +0300

    Stage 5/6

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -11,11 +11,7 @@ def regEx_engine(tupple_of_splitted_string):
 
 # character-by-character comparison
 def comparison_ch_by_ch(regex_expr_ch, input_value_ch):
-    if len(regex_expr_ch) == 0:
-        return True
-    if regex_expr_ch == '.':
-        return True
-    if regex_expr_ch == input_value_ch:
+    if len(regex_expr_ch) == 0 or regex_expr_ch == '.' or regex_expr_ch == input_value_ch:
         return True
     return False
 
@@ -27,6 +23,19 @@ def comparing_expr(regex_expr, input_value):
         return True
     if len(input_value) == 0:
         return False
+    if len(regex_expr) > 1:
+        if (regex_expr[1] == '?' or regex_expr[1] == '*') and comparison_ch_by_ch(regex_expr[0], input_value[0]) is False:
+            return comparing_expr(regex_expr[2:], input_value)
+        if regex_expr[1] == '?' and comparison_ch_by_ch(regex_expr[0], input_value[0]) is True:
+            return comparing_expr(regex_expr[2:], input_value[1:])
+        if regex_expr[1] == '*' or regex_expr[1] == '+':
+            i = 0
+            while comparison_ch_by_ch(regex_expr[0], input_value[i]) is True:
+                i += 1
+                if i >= len(input_value) or input_value[i] != input_value[i - 1]:
+                    break
+            if i > 0:
+                return comparing_expr(regex_expr[2:], input_value[i:])
     # character-by-character comparison
     if comparison_ch_by_ch(regex_expr[0], input_value[0]) is False:
         return False
